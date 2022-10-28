298fff2f0606f104f9e0d2e0200aa7edc2567195
Nacho <ignacio.nahuel.ramos@gmail.com>
2022-10-28 20:17:35 -0300

    added remove duplicates function

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests
+from authorization import authorize
 
-bearer = "BQBHUXcLHBo0f0KBrQHNeUZwvKnrKlwwh5rfgLooJ6UJYVYkR53MW5MAE-MlbaX3EJgkiwnHvoKJQ17spJCDtQ98KeyxycAFtoweQUKcTNXmqLLEKc21FGJFaz30tZZTCJj90SVU2No74eRrCK-nez35Y7FymAQ49ytF_dx271BAxwkfeg"
+client_id = '86df2104482c438d89e25692b2dffc95'
+client_secret = '2f2be1289a9d4287aa15f6b223bdd888'
+
+scope = "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-library-modify user-library-read ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-read-email user-read-private"
+
 main_playlist_id = "6PtI8T8ZBWE4fn4jIPtIyr"
 test_playlist_id = "056NsgfUH3ebxMUoT3sNPw"
+
+spotify = authorize(scope, client_id, client_secret)
+bearer = spotify._auth
 
 
 def request_playlist_items(offset, bearer, playlist_id):
@@ -39,8 +47,6 @@ def get_songs(bearer, playlist_id):
                 break
     return songs
 
-# If they are, delete them from the test playlist
-
 
 def check_for_duplicates(main_playlist_id, test_playlist_id, bearer):
     main_playlist_songs = get_songs(bearer, main_playlist_id)
@@ -54,10 +60,15 @@ def check_for_duplicates(main_playlist_id, test_playlist_id, bearer):
             if main_song['id'] == test_song['id']:
                 duplicate_song['name'] = test_song['name']
                 duplicate_song['id'] = test_song['id']
-                duplicate_song['uri'] = test_song['track']['uri']
+                duplicate_song['uri'] = test_song['uri']
                 duplicates.append(duplicate_song.copy())
 
     return duplicates
 
+def remove_duplicates(main_playlist_id, test_playlist_id, bearer):
+    duplicates = check_for_duplicates(main_playlist_id, test_playlist_id, bearer)
+    for duplicate in duplicates:
+        spotify.playlist_remove_all_occurrences_of_items(test_playlist_id, [duplicate['uri']])
+        print(f"Removed {duplicate['name']}")
 
-print(check_for_duplicates(main_playlist_id, test_playlist_id, bearer))
+print(remove_duplicates(main_playlist_id, test_playlist_id, bearer))
