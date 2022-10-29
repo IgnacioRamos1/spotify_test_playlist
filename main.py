7ab101030877d785daa484dd76e2b3cc138ece64
Nacho <ignacio.nahuel.ramos@gmail.com>
2022-10-29 15:17:44 -0300

    corrected break problems with for loop and changed request function

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 from authorization import authorize
 
 client_id = '86df2104482c438d89e25692b2dffc95'
@@ -10,47 +9,36 @@ main_playlist_id = "6PtI8T8ZBWE4fn4jIPtIyr"
 test_playlist_id = "056NsgfUH3ebxMUoT3sNPw"
 
 spotify = authorize(scope, client_id, client_secret)
-bearer = spotify._auth
 
 
-def request_playlist_items(offset, bearer, playlist_id):
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {bearer}',
-    }
-
-    params = {
-        'offset': f'{offset}',
-    }
-
-    response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
-                            params=params,
-                            headers=headers
-                            )
-
-    return response.json()
+def request_playlist_tracks(playlist_id, offset):
+    playlist_items = spotify.playlist_items(
+                                            playlist_id,
+                                            limit=100,
+                                            offset=offset
+                                            )
+    return playlist_items
 
 
-def get_songs(bearer, playlist_id):
+def get_songs(playlist_id):
     songs = []
     song = {}
     for i in range(0, 10000, 100):
-        items = request_playlist_items(i, bearer, playlist_id)['items']
-        for item in items:
-            if items != []:
+        items = request_playlist_tracks(playlist_id, i)['items']
+        if items != []:
+            for item in items:
                 song['name'] = item['track']['name']
                 song['id'] = item['track']['id']
                 song['uri'] = item['track']['uri']
                 songs.append(song.copy())
-            else:
-                break
+        else:
+            break
     return songs
 
 
-def check_for_duplicates(main_playlist_id, test_playlist_id, bearer):
-    main_playlist_songs = get_songs(bearer, main_playlist_id)
-    test_playlist_songs = get_songs(bearer, test_playlist_id)
+def check_for_duplicates(main_playlist_id, test_playlist_id):
+    main_playlist_songs = get_songs(main_playlist_id)
+    test_playlist_songs = get_songs(test_playlist_id)
 
     duplicates = []
     duplicate_song = {}
@@ -62,13 +50,12 @@ def check_for_duplicates(main_playlist_id, test_playlist_id, bearer):
                 duplicate_song['id'] = test_song['id']
                 duplicate_song['uri'] = test_song['uri']
                 duplicates.append(duplicate_song.copy())
-
     return duplicates
 
-def remove_duplicates(main_playlist_id, test_playlist_id, bearer):
-    duplicates = check_for_duplicates(main_playlist_id, test_playlist_id, bearer)
+
+def remove_duplicates(main_playlist_id, test_playlist_id):
+    duplicates = check_for_duplicates(main_playlist_id, test_playlist_id)
     for duplicate in duplicates:
         spotify.playlist_remove_all_occurrences_of_items(test_playlist_id, [duplicate['uri']])
         print(f"Removed {duplicate['name']}")
 
-print(remove_duplicates(main_playlist_id, test_playlist_id, bearer))
